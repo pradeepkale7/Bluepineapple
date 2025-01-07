81cab24401565f80f4a04afed6053074a4d57993
Pradeep Kale <kalepradeep2001@gmail.com>
2025-01-07 15:30:40 +0530

    Create PerfectNumber.py
    Code for perfect number

---
+++ PerfectNumber.py
@@ -0,0 +1,12 @@
+
+n =int(input("Enter the range of numbers : "))
+
+for num in range(1,n):    # for 1 to n
+    sum=0  # for checking the final value 
+    
+    for i in range(1,num):    #for checking the quotient
+    
+        if(num%i==0):
+            sum+=i           # adding the quotient sum 
+        
+    if(sum==num): print(f"{sum} ")  # printing the perfect number 
