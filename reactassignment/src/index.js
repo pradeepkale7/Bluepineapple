cde24f40fdd4a52c98afffb4e66ddfeb15579907
PradeepKale7 <kalepradeep2001@gmail.com>
2025-02-04 12:04:29 +0530

    React Assignment done in reactassignment Folder

--- reactassignment/src/index.js
+++ reactassignment/src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import App from './index5';
 import UserData from './index1';
 import InputFocus from './index2';
 import TodoList from './index3';
@@ -11,10 +10,10 @@ import ParentChild from './index4';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    {/* <App /> */}
-    {/* <UserData /> */}
-    {/* <InputFocus/> */}
-    {/* <TodoList/> */}
+    {/* <App />
+    <UserData />
+    <InputFocus/>
+    <TodoList/> */}
     <ParentChild/>
   </React.StrictMode>
 );
